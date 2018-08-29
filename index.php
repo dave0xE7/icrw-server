43dfcddbf7821bf1c27e0592f1780040d7ebd454
root <root@localhost.localdomain>
2018-08-29 23:17:03 +0200

    init

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once("EasyBitcoin-PHP/easybitcoin.php");
+
+$intercrone = new Bitcoin("InterCronerpc", "1337133713371337", "localhost", "8443");
+
+#$info = $intercrone->getinfo();
+$info = $intercrone->listtransactions();
+
+print_r ($info);
+
+?>
